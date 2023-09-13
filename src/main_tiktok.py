cbe537ee3b71cdf28e10ce56ae2db5fb3056cbfb
一刀 <yhonglai@163.com>
2023-09-13 22:49:22 +0800

    perf 去除secrets参数

--- src/main_tiktok.py
+++ src/main_tiktok.py
@@ -1,29 +1,33 @@
 import tiktok_utils
 import weixin
 import os
+import config
 
 if __name__ == "__main__":
     try:
-        tiktok_cookie = os.environ['TIKTOK_COOKIE']
-        wxpusher_token = os.environ['WXPUSHER_TOKEN']
-        wechat_uid = os.environ['WECHAT_UID']
-        accounts = os.environ['TIKTOK_ACCOUNTS']
+        if config.config("switch_run"):
+            tiktok_cookie = config.config["tiktok_cookie"]
+            wxpusher_token = config.config["wxpusher_token"]
+            wechat_uid = os.environ['WECHAT_UID']
+            accounts = os.environ['TIKTOK_ACCOUNTS']
 
-        list = accounts.split(',')
-        users = []
-        result = ""
-        for item in list:
-            info = tiktok_utils.getUserInfo(item, tiktok_cookie)
-            if info == None:
-                print("获取用户信息失败")
-                result += "用户信息丢失:"+item
-            else:
-                users.append(info)
-                print(item)
-                result += info.toString()
+            list = accounts.split(',')
+            users = []
+            result = ""
+            for item in list:
+                info = tiktok_utils.getUserInfo(item, tiktok_cookie)
+                if info == None:
+                    print("获取用户信息失败")
+                    result += "用户信息丢失:"+item
+                else:
+                    users.append(info)
+                    print(item)
+                    result += info.toString()
 
-        weixin.wxpusher_send_by_webapi(
-            result, "TikTok日报", wxpusher_token, wechat_uid)
+            weixin.wxpusher_send_by_webapi(
+                result, "TikTok日报", wxpusher_token, wechat_uid)
+        else:
+            print('本次不执行')
     except Exception as err:
         print("运行错误")
         print(err)
