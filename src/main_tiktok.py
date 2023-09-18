f82b112728254ec2c0154306ec2c5bc060b6673b
一刀 <yhonglai@163.com>
2023-09-18 23:47:28 +0800

    feat 支持计算变化数据

--- src/main_tiktok.py
+++ src/main_tiktok.py
@@ -2,6 +2,9 @@ import tiktok_utils
 import weixin
 import os
 import config
+import json
+
+file_path = 'data.json'
 
 if __name__ == "__main__":
     try:
@@ -10,6 +13,14 @@ if __name__ == "__main__":
             wxpusher_token = config.user_config['wxpusher_token']
             wechat_uid = os.environ['WECHAT_UID']
             accounts = os.environ['TIKTOK_ACCOUNTS']
+
+            data = None
+            if os.path.exists(file_path):
+                with open(file_path, 'r') as json_file:
+                    data = json.load(json_file)
+                    print(data)
+            else:
+                print(f"The file {file_path} does not exist.")
 
             list = accounts.split(',')
             users = []
@@ -22,10 +33,30 @@ if __name__ == "__main__":
                 else:
                     users.append(info)
                     print(item)
-                    result += info.toString()
+
+                    yesterday = None
+                    try:
+                        if data is not None:
+                            for tmpItem in data:
+                                if tmpItem.get("username") == info.username:
+                                    yesterday = tmpItem
+                                    break
+                    except Exception as err:
+                        print('获取对比信息错误')
+                        print(err)
+
+                    if yesterday == None:
+                        result += info.toString()
+                    else:
+                        result += info.toString(yesterday.followCount, yesterday.fansCount,
+                                                yesterday.likeCount, yesterday.videoCount)
 
             weixin.wxpusher_send_by_webapi(
                 result, "TikTok日报", wxpusher_token, wechat_uid)
+
+            # 保存文件
+            with open(file_path, 'w') as json_file:
+                json.dump(result, json_file)
         else:
             print('本次不执行')
     except Exception as err:
--- src/models.py
+++ src/models.py
@@ -1,5 +1,6 @@
 import enums
 
+
 class UserInfo:
     def __init__(self, username: str, nickname: str, platform: enums.PlatformType, date, followCount, fansCount, likeCount, videoCount) -> None:
         self.username = username
@@ -10,7 +11,6 @@ class UserInfo:
         self.fansCount = fansCount
         self.likeCount = likeCount
         self.videoCount = videoCount
-        pass
 
     def toString(self):
         result = """账号：{0}
@@ -18,6 +18,14 @@ class UserInfo:
 粉丝数量：{2}
 点赞数量：{3}
 视频数量：{4}
-
 """.format(self.nickname, self.followCount, self.fansCount, self.likeCount, self.videoCount)
         return result
+
+    def toString(self, yesterdayFollow, yseterdatFans, yesterdayLike, yesterdayVideo):
+        reslut = self.toString()
+        reslut += """关注数量变化：{0}
+粉丝数量变化：{1}
+点赞数量变化：{2}
+视频数量变化：{3}
+
+""".format(self.followCount-yesterdayFollow, self.fansCount-yseterdatFans, self.likeCount-yesterdayLike, self.videoCount-yesterdayVideo)
