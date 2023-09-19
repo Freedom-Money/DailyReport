9991680b87274058e6dc0d78d547436d04e255f4
一刀 <yhonglai@163.com>
2023-09-19 23:09:00 +0800

    fix 修复tostring异常

--- ReadMe.md
+++ ReadMe.md
@@ -18,3 +18,9 @@
 ## 3. 设置被监控账号列表
 
 1. 添加变量，名称为【TIKTOK_ACCOUNTS】，内容为用户id列表（以英文逗号间隔），如“yuandao5,yuandd39,yuanaab,yuanyuan4096”。
+
+## 4. 设置Github Token
+
+1. 用户中心选择"Settings"、"Developer settings"，然后选择 "Personal access tokens"、"Tokens(classic)"。点击 "Generate new token"，选择需要的权限（通常需要 repo 权限，如果不确定就全选）。
+2. 复制生成的token。
+3. GitHub 仓库中，点击右上角的 "Settings"。在左侧导航栏中选择 "Secrets"，然后点击 "New repository secret"。给 secret 一个名称（GH_TOKEN），将之前生成的 Personal Access Token 复制粘贴到 "Value" 字段中。保存
--- src/models.py
+++ src/models.py
@@ -12,20 +12,20 @@ class UserInfo:
         self.likeCount = likeCount
         self.videoCount = videoCount
 
-    def toString(self):
+    def toString(self, yesterdayFollow=None, yseterdatFans=None, yesterdayLike=None, yesterdayVideo=None):
         result = """账号：{0}
 关注数量：{1}
 粉丝数量：{2}
 点赞数量：{3}
 视频数量：{4}
 """.format(self.nickname, self.followCount, self.fansCount, self.likeCount, self.videoCount)
-        return result
 
-    def toString(self, yesterdayFollow, yseterdatFans, yesterdayLike, yesterdayVideo):
-        reslut = self.toString()
-        reslut += """关注数量变化：{0}
+        if yesterdayFollow is not None:
+            reslut += """关注数量变化：{0}
 粉丝数量变化：{1}
 点赞数量变化：{2}
 视频数量变化：{3}
 
 """.format(self.followCount-yesterdayFollow, self.fansCount-yseterdatFans, self.likeCount-yesterdayLike, self.videoCount-yesterdayVideo)
+
+        return result
